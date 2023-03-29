f53354576da851a8b743f6818b9e3a9388cc879a
Serge Schneider <serge@raspberrypi.com>
2023-03-29 08:12:54 +0100

    pi5 support

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -15,10 +15,16 @@ if [ -v WPA_COUNTRY ]; then
 	on_chroot <<- EOF
 		SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_country "${WPA_COUNTRY}"
 	EOF
-elif [ -d "${ROOTFS_DIR}/var/lib/NetworkManager" ]; then
-	# NetworkManager unblocks all WLAN devices by default. Prevent that:
-	cat > "${ROOTFS_DIR}/var/lib/NetworkManager/NetworkManager.state" <<- EOF
-		[main]
-		WirelessEnabled=false
-	EOF
+fi
+
+# Disable wifi on 5GHz models if WPA_COUNTRY is not set
+mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
+if [ -n "$WPA_COUNTRY" ]; then
+    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
+    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
+    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-1001100000.mmc:wlan"
+else
+    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
+    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
+    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-1001100000.mmc:wlan"
 fi

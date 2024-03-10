a51d6bd0b1ada42a860e69254f0e35bbea7ce52e
Darrick Servis <darrick@davismedia.org>
2024-03-09 18:44:52 -0800

    Update package keys.

--- stage2/04-custom/00-run.sh
+++ stage2/04-custom/00-run.sh
@@ -4,10 +4,14 @@ install -m 644 files/.gitconfig	"${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.gitco
 
 install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
+install -d "${ROOTFS_DIR}/etc/apt/keyrings/"
 cat files/docker.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/docker.gpg"
-install -m 644 "${STAGE_WORK_DIR}/docker.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+install -m 644 "${STAGE_WORK_DIR}/docker.gpg" "${ROOTFS_DIR}/etc/apt/keyrings/"
 
 install -m 644 files/openvpn3.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-cat files/openvpn3.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/openvpn3.gpg"
-install -m 644 "${STAGE_WORK_DIR}/openvpn3.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+install -m 644 files/openvpn3.gpg.key "${ROOTFS_DIR}/etc/apt/keyrings/openvpn.asc"
+
+on_chroot << EOF
+apt-get update
+EOF
